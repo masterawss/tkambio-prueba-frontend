5d0ab1a82bae041b628dbf97dcf182a6297aeaa3
Anthony Will Solsol Soplin <masterawss@gmail.com>
2022-01-21 15:28:42 -0500

    to Hash mode

--- src/utils/apiController.js
+++ src/utils/apiController.js
@@ -1,5 +1,5 @@
 import {BASE_URL} from './axios'
 
 export const getReport = (id) => {
-  window.open(`${BASE_URL}api/get-report/${id}`)
+  window.open(`https://tkambio-prueba.herokuapp.com/public/api/get-report/${id}`)
 }
